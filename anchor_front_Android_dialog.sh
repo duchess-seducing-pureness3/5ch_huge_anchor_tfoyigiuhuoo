ae2ad6f8e72f172821a7ada399edee75607ff5ba
mint <mint@dssdsfaeaw.rleiangofewfewfeww>
2021-08-26 02:13:47 +0900

    ?

--- anchor_front_Android_dialog.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# Termuxのダイアログ用
-# 通知欄に表示してタップでコピー(多分Android 11とかでも動くはず(未確認))
-input="$(termux-dialog -t "アンカーを入力してください" -i "アンカーを飛ばしたい先の数字を入力" -n)"
-input_num="(echo $input | jq -r .text)"
-SCRIPT_ANCHOR_DIR="./anchor.sh"
-anchor="($SCRIPT_ANCHOR_DIR $input_num)"
-termux-notification  --action "(termux-clipboard-set ""$anchor"" && termux-toast $anchorをコピーしました)" -t "大きな数字のアンカー" -c "タップして「$anchor」をコピーします"
