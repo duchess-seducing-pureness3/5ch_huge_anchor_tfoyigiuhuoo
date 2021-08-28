14a63300332876e0630e3da336c8ca7dc5ebf7e6
mint <mint@dssdsfaeaw.rleiangofewfewfeww>
2021-08-28 12:14:36 +0900

    typo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 apt install jq python -y
 
 # ウィジェット用のディレクトリ作成しコピーし実行権限を付与
-mksir $HOME/.shortcut/tasks
+mkdir $HOME/.shortcut/tasks
 cp $HOME/5ch_huge_anchor_tfoyigiuhuoo/大きな数字のアンカーを生成 $HOME/.shortcut/tasks
 chmod +x $HOME/.shortcut/tasks/大きな数字のアンカーを生成
 # 各スクリプトに実行権限を付与
