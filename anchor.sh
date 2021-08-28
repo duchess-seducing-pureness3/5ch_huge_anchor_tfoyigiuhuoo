bfe2086501e740fcee5d6adc55cf5632f8480383
mint <mint@dssdsfaeaw.rleiangofewfewfeww>
2021-08-28 13:14:59 +0900

    ノイズ除去

--- anchor.sh
+++ anchor.sh
@@ -5,4 +5,5 @@ tmp_num=$1
 PYTHON_SCRIPT_DIR="$CURRENT"'/new_type_ancor.py'
 
 tmp_anc=$(python3 $PYTHON_SCRIPT_DIR "$tmp_num")
-printf '%sをコピーしました\n' "$tmp_anc"
+echo "$tmp_anc"
+# printf '%sをコピーしました\n' "$tmp_anc"
