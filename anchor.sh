8716cff4ba19c3ade0f900ea27a7f93d4d875678
mint <mint@dssdsfaeaw.rleiangofewfewfeww>
2021-08-28 12:49:48 +0900

    余計な"の削除

--- anchor.sh
+++ anchor.sh
@@ -3,5 +3,5 @@
 tmp_num=$1
 PYTHON_SCRIPT_DIR='./new_type_ancor.py'
 
-tmp_anc="$(python3 $PYTHON_SCRIPT_DIR "$tmp_num")"
+tmp_anc=$(python3 $PYTHON_SCRIPT_DIR "$tmp_num")
 printf '%sをコピーしました\n' "$tmp_anc"
--- anchor_front_Android_clibord.sh
+++ anchor_front_Android_clibord.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # クリップボード用のスクリプト
 # クリップボードの値を取得して計算後セットする
-num="$(termux-clipboard-get)"
+num=$(termux-clipboard-get)
 SCRIPT_ANCHOR_DIR="./anchor.sh"
-anchor="$($SCRIPT_ANCHOR_DIR $num)"
+anchor=$($SCRIPT_ANCHOR_DIR $num)
 termux-clipboard-set "$anchor"
