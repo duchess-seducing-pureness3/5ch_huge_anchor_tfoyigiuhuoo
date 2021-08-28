19e8a1689b2030357fbdca90c84063a9620c06b2
mint <mint@dssdsfaeaw.rleiangofewfewfeww>
2021-08-28 13:34:05 +0900

    スクリプトからtermux-clipboard-setが使えないので修正

--- anchor_front_Android_dialog.sh
+++ anchor_front_Android_dialog.sh
@@ -7,4 +7,4 @@ input=$(termux-dialog -t "アンカーを入力してください" -i "アンカ
 input_num=$(echo $input | jq -r .text)
 SCRIPT_ANCHOR_DIR="$CURRENT/anchor.sh"
 anchor=$($SCRIPT_ANCHOR_DIR $input_num)
-termux-notification  --action "(termux-clipboard-set ""$anchor"" && termux-toast ""$anchor""をコピーしました)" -t "大きな数字のアンカー" -c "タップして「$anchor」をコピーします"
+termux-notification  --action "(termux-clipboard-set "$anchor" && termux-toast "$anchor"をコピーしました)" -t "大きな数字のアンカー" -c "タップして「$anchor」をコピーします" --button1-action "(termux-clipboard-set "$anchor" && termux-toast "$anchor"をコピーしました)"  --button1 "コピー"
