6444c02364a194e5b324d704d33ee715c1cfa3fe
mint <mint@dssdsfaeaw.rleiangofewfewfeww>
2021-08-28 12:42:24 +0900

    括弧周りを変更

--- anchor_front_Android_clibord.sh
+++ anchor_front_Android_clibord.sh
@@ -3,5 +3,5 @@
 # クリップボードの値を取得して計算後セットする
 num="$(termux-clipboard-get)"
 SCRIPT_ANCHOR_DIR="./anchor.sh"
-anchor="($SCRIPT_ANCHOR_DIR $num)"
+anchor="$($SCRIPT_ANCHOR_DIR $num)"
 termux-clipboard-set "$anchor"
