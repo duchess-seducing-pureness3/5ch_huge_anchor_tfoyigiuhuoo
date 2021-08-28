12ee43fe2ea7ec1fe52796541fc4b52af076890f
mint <mint@dssdsfaeaw.rleiangofewfewfeww>
2021-08-28 15:25:07 +0900

    waitを外した

--- anchor_front_Android_dialog.sh
+++ anchor_front_Android_dialog.sh
@@ -4,11 +4,7 @@
 CURRENT=$(cd $(dirname $0);pwd)
 
 input=$(termux-dialog -t "アンカーを入力してください" -i "アンカーを飛ばしたい先の数字を入力" -n)
-wait
 input_num=$(echo $input | jq -r .text)
 SCRIPT_ANCHOR_DIR="$CURRENT/anchor.sh"
 anchor=$($SCRIPT_ANCHOR_DIR $input_num)
-termux-notification  --action "termux-clipboard-set $anchor ; termux-toast $anchorをコピーしました" \
-    -t 大きな数字のアンカー -c タップして「$anchor」をコピーします \
-    --button1-action "termux-clipboard-set $anchor ; termux-toast $anchorをコピーしました ; termux-vibrate" \
-    --button1 コピー
+termux-notification  --action "termux-clipboard-set $anchor ; termux-toast $anchorをコピーしました" -t 大きな数字のアンカー -c タップして「$anchor」をコピーします --button1-action "termux-clipboard-set $anchor ; termux-toast $anchorをコピーしました ; termux-vibrate" --button1 コピー
