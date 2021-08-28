ddd8b1cda551979c8aa40499d393d3976bb8f23d
mint <mint@dssdsfaeaw.rleiangofewfewfeww>
2021-08-28 16:40:52 +0900

    ウィジェット周りの挙動がおかしくなるので修正

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ apt install jq python -y
 mkdir -p $HOME/.shortcuts/tasks
 cp $HOME/5ch_huge_anchor_tfoyigiuhuoo/大きな数字のアンカーを生成 $HOME/.shortcuts/tasks
 chmod +x $HOME/.shortcuts/tasks/大きな数字のアンカーを生成
+# termux-execでshebangは直さなくても原則動くはずだがなぜかウィジェットに置くと起こられたり挙動がおかしくなる
+# また直したほうが実行速度が早い
+termux-fix-shebang $HOME/.shortcuts/tasks/大きな数字のアンカーを生成
 # 各スクリプトに実行権限を付与
 list=(
   $HOME/5ch_huge_anchor_tfoyigiuhuoo/anchor_front_Android_dialog.sh
@@ -17,4 +20,5 @@ list=(
 );
 for name in ${list[@]}; do
 chmod +x $name
+termux-fix-shebang $name
 done
